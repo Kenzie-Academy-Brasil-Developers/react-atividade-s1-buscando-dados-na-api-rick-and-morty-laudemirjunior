cf966d10f490aca1f560c5b1091d2561611a9b94
Laudemir <nlaudemir@gmail.com>
2021-09-08 01:33:28 -0300

    entrega de atividade

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import Characters from "./components/Characters";
+
+function App() {
+  const [characterList, setCharacterList] = useState([]);
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character/")
+      .then((response) => response.json())
+      .then((response) => setCharacterList(response.results))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="App">
+      <Characters characterList={characterList} />
+    </div>
+  );
+}
+
+export default App;
